f3a9dd33e2f4e68723e6478d709bb4f801c2b2d9
Mohamed AbdelraZek <muhamedrabi3@gmail.com>
2017-03-02 15:16:48 +0200

    Adding animation on  favourite imageButton click

--- app/src/main/java/com/zoka/moviesapp/DetailsFragment.java
+++ app/src/main/java/com/zoka/moviesapp/DetailsFragment.java
@@ -17,6 +17,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -55,7 +58,7 @@ public class DetailsFragment extends Fragment {
     private TrailersAdapter mTrailersAdapter;
 
     @BindView(R.id.fav)
-    ImageView favorite;
+    ImageButton favorite;
     @BindView(R.id.ratingBar)
     RatingBar mRate;
     @BindView(R.id.back_drop_path)
@@ -110,18 +113,20 @@ public class DetailsFragment extends Fragment {
             mTrailerTitle.setVisibility(View.VISIBLE);
 
         }
-
+        final Animation anim = AnimationUtils.loadAnimation(getContext(), R.anim.anim);
         favorite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                favorite.startAnimation(anim);
                 ChangeFavouriteMovies();
+
             }
         });
 
         return zRootView;
     }
 
-    private  void ChangeFavouriteMovies() {
+    private void ChangeFavouriteMovies() {
         if (iSInFavouriteList()) {
             deleteMovie();
             favorite.setImageResource(R.drawable.heart_not_fav);
