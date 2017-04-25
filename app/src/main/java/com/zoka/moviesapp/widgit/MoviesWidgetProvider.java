229a859830a809ea637a9e50e94a10badaa45897
Mohamed AbdelraZek <muhamedrabi3@gmail.com>
2017-04-25 23:46:17 +0200

    adding widjit

---
+++ app/src/main/java/com/zoka/moviesapp/widgit/MoviesWidgetProvider.java
@@ -0,0 +1,87 @@
+package com.zoka.moviesapp.widgit;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.widget.RemoteViews;
+
+import com.zoka.moviesapp.DetailsActivity;
+import com.zoka.moviesapp.MainActivity;
+import com.zoka.moviesapp.R;
+
+
+public class MoviesWidgetProvider extends AppWidgetProvider {
+    @SuppressLint("PrivateResource")
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int widgetId : appWidgetIds) {
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_movies_list);
+
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            remoteViews.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+            // Set up the collection
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+                setRemoteAdapter(context, remoteViews);
+            } else {
+                setRemoteAdapterV11(context, remoteViews);
+            }
+
+            Intent clickIntentTemplate = new Intent(context, DetailsActivity.class);
+
+            PendingIntent pendingIntentTemplate = TaskStackBuilder.create(context)
+                    .addNextIntentWithParentStack(clickIntentTemplate)
+                    .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            remoteViews.setPendingIntentTemplate(R.id.widget_list, pendingIntentTemplate);
+            remoteViews.setEmptyView(R.id.widget_list, R.id.widget_empty);
+            remoteViews.setInt(R.id.widget_list, "setBackgroundResource", R.color.material_grey);
+            remoteViews.setInt(R.id.widget_content, "setBackgroundResource", R.color.material_grey);
+            remoteViews.setContentDescription(R.id.widget_list, context.getString(R.string.widget_cd));
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(
+                    new ComponentName(context, getClass()));
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_list);
+        }
+
+
+    /**
+     * Sets the remote adapter used to fill in the list items
+     *
+     * @param views RemoteViews to set the RemoteAdapter
+     */
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    private void setRemoteAdapter(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(R.id.widget_list,
+                new Intent(context, MoviesWidgetService.class));
+    }
+
+    /**
+     * Sets the remote adapter used to fill in the list items
+     *
+     * @param views RemoteViews to set the RemoteAdapter
+     */
+    @SuppressWarnings("deprecation")
+    private void setRemoteAdapterV11(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(0, R.id.widget_list,
+                new Intent(context, MoviesWidgetService.class));
+    }
+}
---
+++ app/src/main/java/com/zoka/moviesapp/widgit/MoviesWidgetService.java
@@ -0,0 +1,117 @@
+package com.zoka.moviesapp.widgit;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.os.Binder;
+import android.os.Handler;
+import android.widget.AdapterView;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.squareup.picasso.Picasso;
+import com.zoka.moviesapp.MoviesFragment;
+import com.zoka.moviesapp.R;
+import com.zoka.moviesapp.data.MoviesContract;
+import com.zoka.moviesapp.utils.PreferenceUtilities;
+
+import java.io.IOException;
+
+
+public class MoviesWidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new ListRemoteViewFactory();
+    }
+
+    public class ListRemoteViewFactory implements RemoteViewsFactory {
+
+        private Cursor data = null;
+
+        //Lifecycle start
+        @Override
+        public void onCreate() {
+            //No action needed
+        }
+
+        @Override
+        public void onDestroy() {
+            if (data != null) {
+                data.close();
+                data = null;
+            }
+
+        }
+        //Lifecycle end
+        @Override
+        public void onDataSetChanged() {
+            if (data != null) data.close();
+
+            final long identityToken = Binder.clearCallingIdentity();
+            data = getContentResolver().query(MoviesContract.MoviesEntry.CONTENT_URI,
+                    MoviesFragment.Movies_PROJECTION,
+                    MoviesContract.MoviesEntry.COLUMN_SORT_TYPE + "= ?",
+                    new String[]{PreferenceUtilities.getSortType(getBaseContext())},
+                    null);
+            Binder.restoreCallingIdentity(identityToken);
+        }
+
+        @Override
+        public int getCount() {
+            return data == null ? 0 : data.getCount();
+        }
+
+        @SuppressLint("PrivateResource")
+        @Override
+        public RemoteViews getViewAt(int position) {
+            if (position == AdapterView.INVALID_POSITION || data == null
+                    || !data.moveToPosition(position)) {
+                return null;
+            }
+
+            final RemoteViews remoteViews = new RemoteViews(getPackageName(),
+                    R.layout.movie_single_item);
+
+            final String imageUrl = data.getString(data.getColumnIndex(MoviesFragment.MOVIES_POSTER_PATH));
+            new Handler().post(new Runnable() {
+                @Override
+                public void run() {
+
+                    Bitmap b = null;
+                    try {
+                        b = Picasso.with(getBaseContext()).load(imageUrl).get();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    remoteViews.setImageViewBitmap(R.id.poster_img_view, b);
+
+
+
+                }
+            });
+            return remoteViews;
+
+        }
+
+        @Override
+        public RemoteViews getLoadingView() {
+            return null;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 1;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return data.moveToPosition(i) ? data.getLong(0) : i;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+    }
+}
